fa386fba9be93d0cc41352fc61f1eefa1abbea94
Nathan Froyd <froydnj@gmail.com>
2010-01-13 22:49:54 -0500

    first crack at async encoding interface for base64

--- base64.lisp
+++ base64.lisp
@@ -8,6 +8,141 @@
 (defvar *base64url-encode-table*
   #.(coerce "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_" 'simple-base-string))
 
+(defstruct (base64-encode-state
+             (:include encode-state)
+             (:copier nil)
+             (:constructor make-base64-encode-state
+                           (table
+                            &aux (encoded-length #'encoded-length/base64)
+                                 (octets->octets #'octets->octets/base64)
+                                 (octets->string #'octets->string/base64))))
+  (bits 0 :type (unsigned-byte 16))
+  (n-bits 0 :type fixnum)
+  (table *base64-encode-table* :read-only t :type (simple-array base-char (64)))
+  (adding-padding-p nil)
+  (padding-remaining 0 :type (integer 0 3)))
+
+(declaim (inline base64-encoder))
+(defun base64-encoder (state output input
+                       output-start output-end
+                       input-start input-end lastp converter)
+  (declare (type base64-encode-state state))
+  (declare (type simple-octet-vector input))
+  (declare (type index output-start output-end input-start input-end))
+  (declare (type function converter))
+  (declare (optimize speed))
+  (let* ((input-index input-start)
+         (output-index output-start)
+         (bits (base64-encode-state-bits state))
+         (n-bits (base64-encode-state-n-bits state))
+         (table (base64-encode-state-table state)))
+    (declare (type index input-index output-index))
+    (declare (type (unsigned-byte 16) bits))
+    (declare (type fixnum n-bits))
+    (tagbody
+     PAD-CHECK
+       (when (base64-encode-state-adding-padding-p state)
+         (go PAD))
+     INPUT-CHECK
+       (when (>= input-index input-end)
+         (go DONE))
+     DO-INPUT
+       (when (< n-bits 6)
+         (setf bits (ldb (byte 16 0)
+                         (logior (ash bits 8) (aref input input-index))))
+         (incf input-index)
+         (incf n-bits 8))
+     OUTPUT-CHECK
+       (when (>= output-index output-end)
+         (go DONE))
+     DO-OUTPUT
+       (decf n-bits 6)
+       (setf (aref output output-index)
+             (funcall converter (aref table (ldb (byte 6 n-bits) bits))))
+       (incf output-index)
+       (if (>= n-bits 6)
+           (go OUTPUT-CHECK)
+           (go INPUT-CHECK))
+     DONE
+       (unless lastp
+         (go RESTORE-STATE))
+       (setf (base64-encode-state-adding-padding-p state) t)
+       (cond
+         ((= n-bits 2)
+          (setf (base64-encode-state-padding-remaining state) 3))
+         ((= n-bits 4)
+          (setf (base64-encode-state-padding-remaining state) 2)))
+     PAD
+       (cond
+         ((or (zerop n-bits)
+              (zerop (base64-encode-state-padding-remaining state)))
+          (go RESTORE-STATE))
+         ((= n-bits 2)
+          (go DO-PAD-FOR-2-BITS))
+         ((= n-bits 4)
+          (go DO-PAD-FOR-4-BITS)))
+     DO-PAD-FOR-2-BITS
+       (let ((padding-remaining (base64-encode-state-padding-remaining state)))
+         (declare (type (integer 0 3) padding-remaining))
+         (when (and (>= padding-remaining 3)
+                    (< output-index output-end))
+           (setf (aref output output-index)
+                 (funcall converter
+                          (aref table (ash (ldb (byte 2 0) bits) 4))))
+           (incf output-index)
+           (decf padding-remaining))
+         (when (and (>= padding-remaining 2)
+                    (< output-index output-end))
+           (setf (aref output output-index) (funcall converter #\=))
+           (incf output-index)
+           (decf padding-remaining))
+         (when (and (>= padding-remaining 1)
+                    (< output-index output-end))
+           (setf (aref output output-index) (funcall converter #\=))
+           (incf output-index)
+           (decf padding-remaining))
+         (when (zerop padding-remaining)
+           (setf n-bits 0))
+         (setf (base64-encode-state-padding-remaining state) padding-remaining)
+         (go RESTORE-STATE))
+     DO-PAD-FOR-4-BITS
+       (let ((padding-remaining (base64-encode-state-padding-remaining state)))
+         (declare (type (integer 0 3) padding-remaining))
+         (when (and (>= padding-remaining 2)
+                    (< output-index output-end))
+          (setf (aref output output-index)
+                (funcall converter
+                         (aref table (ash (ldb (byte 4 0) bits) 2))))
+          (incf output-index)
+          (decf padding-remaining))
+         (when (and (>= padding-remaining 1)
+                    (< output-index output-end))
+           (setf (aref output output-index) (funcall converter #\=))
+           (incf output-index)
+           (decf padding-remaining))
+         (when (zerop padding-remaining)
+           (setf n-bits 0))
+         (setf (base64-encode-state-padding-remaining state) padding-remaining)
+         (go RESTORE-STATE))
+     RESTORE-STATE
+       (setf (base64-encode-state-bits state) bits
+             (base64-encode-state-n-bits state) n-bits))
+    (values (- input-index input-start) (- output-index output-start))))
+
+(defun octets->octets/base64 (state output input
+                              output-start output-end
+                              input-start input-end lastp)
+  (declare (type simple-octet-vector output))
+  (base64-encoder state output input output-start output-end
+                  input-start input-end lastp #'char-code))
+
+(defun octets->string/base64 (state output input
+                              output-start output-end
+                              input-start input-end lastp)
+  (declare (type simple-string output))
+  (base64-encoder state output input output-start output-end
+                  input-start input-end lastp #'identity))
+
 (defun encode-octets-base64 (octets start end table writer)
   (declare (type (simple-array (unsigned-byte 8) (*)) octets))
   (declare (type index start end))
@@ -30,16 +165,16 @@
                 (funcall writer (aref table (ash (ldb (byte 4 0) bits) 2)))
                 (funcall writer #\=)))))
 
-(defun encoded-length-base64 (count)
+(defun encoded-length/base64 (count)
   "Return the number of characters required to encode COUNT octets in Base64."
   (* (ceiling count 3) 4))
 
 (defmethod encoding-tools ((format (eql :base64)))
-  (values #'encode-octets-base64 #'encoded-length-base64
+  (values #'encode-octets-base64 #'encoded-length/base64
           *base64-encode-table*))
 
 (defmethod encoding-tools ((format (eql :base64url)))
-  (values #'encode-octets-base64 #'encoded-length-base64
+  (values #'encode-octets-base64 #'encoded-length/base64
           *base64url-encode-table*))
 
 (defvar *base64-decode-table*
