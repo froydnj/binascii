a446e1fe6b2edba1576eb6f92e13b695815a680c
Nathan Froyd <froydnj@gmail.com>
2010-01-17 08:37:59 -0500

    add async encode functions for base85

--- base85.lisp
+++ base85.lisp
@@ -21,15 +21,131 @@
   ;;
   ;; For now, though, we'll fill GROUP front-to-back and PENDING will
   ;; indicate how many octets we've filled in.
+  #+nil
   (group (make-array 4 :element-type '(unsigned-byte 8))
          :read-only t :type (simple-array (unsigned-byte 8) (4)))
+  (bits 0 :type (unsigned-byte 32))
   (pending 0 :type (integer 0 4))
+  (output-group (make-array 5 :element-type 'base-char)
+                :read-only t :type (simple-array base-char (5)))
+  (output-pending 0 :type (integer 0 5))
   (table *base85-encode-table* :read-only t :type (simple-array base-char (85)))
   (finished-input-p nil))
 
 (defun encoded-length/base85 (count)
   "Return the number of characters required to encode COUNT octets in Base85."
   (* (ceiling count 4) 5))
+
+(declaim (inline base85-encode))
+(defun base85-encoder (state output input
+                       output-start ouput-end
+                       input-start input-end lastp converter)
+  (declare (type base85-encode-state state))
+  (declare (type simple-octet-vector input))
+  (declare (type index output-start output-end input-start input-end))
+  (declare (type function converter))
+  (let ((input-index input-start)
+        (output-index output-start)
+        (bits (base85-encode-state-bits state))
+        (pending (base85-encode-state-pending state))
+        (output-group (base85-encode-state-output-group state))
+        (output-pending (base85-encode-state-output-pending state))
+        (table (base85-encode-state-table state)))
+    (declare (type index input-index output-index))
+    (declare (type (unsigned-byte 32) bits))
+    (declare (type (integer 0 4) pending))
+    (declare (type (integer 0 5) output-pending))
+    (flet ((expand-for-output (bits output-group)
+             (loop for i from 4 downto 0
+                do (multiple-value-bind (b index) (truncate bits 85)
+                     (setf bits b
+                           (aref output-group i) (aref table index)))
+                finally (setf output-pending 5))))
+      (declare (inline expand-for-output))
+      (tagbody
+       PAD-CHECK
+         (when (base85-encoder-state-finished-input-p state)
+           (go FLUSH-BITS))
+       INPUT-CHECK
+         (when (>= input-index input-end)
+           (go DONE))
+       DO-INPUT
+         (when (< pending 4)
+           (setf bits (ldb (byte 32 0)
+                           (logior (ash (aref input input-index)
+                                        (- 24 (* pending 8)))
+                                   bits)))
+           (incf input-index)
+           (incf pending)
+           (go INPUT-CHECK))
+       EXPAND-FOR-OUTPUT
+         (expand-for-output bits output-group)
+       OUTPUT-CHECK
+         (when (>= output-index output-end)
+           (go DONE))
+       DO-OUTPUT
+         (when (> output-pending 0)
+           (setf (aref output output-index)
+                 (funcall converter
+                          (aref output-group (decf output-pending))))
+           (incf output-index)
+           (cond
+             ((zerop output-pending)
+              (setf bits 0)
+              (setf pending 0)
+              (go INPUT-CHECK))
+             (t
+              (go OUTPUT-CHECK))))
+       DONE
+         (unless lastp
+           (go RESTORE-STATE))
+         (setf (base85-encode-state-finished-input-p state) t)
+         ;; Make it appear as though the input were padded with zeros to a
+         ;; full input group.
+         (let ((for-pad (- 4 pending)))
+           (setf bits (ldb (byte 32 0) (ash bits (* 8 for-pad))))
+           (setf pending 4)
+           (expand-for-output bits output-group))
+       FLUSH-BITS
+         (when (zerop output-pending)
+           (go RESTORE-STATE))
+       FLUSH-OUTPUT-CHECK
+         (when (>= output-index output-end)
+           (go RESTORE-STATE))
+       DO-FLUSH-OUTPUT
+         (when (> output-pending 0)
+           (setf (aref output output-index)
+                 (funcall converter
+                          (aref output-group (decf output-pending))))
+           (incf output-index)
+           (cond
+             ((zerop output-pending)
+              (setf bits 0)
+              (setf pending 0)
+              (go RESTORE-STATE))
+             (t
+              (go OUTPUT-CHECK))))
+       RESTORE-STATE
+         (setf (base85-encode-state-bits state) bits
+               (base85-encode-state-pending state) pending
+               (base85-encode-state-output-pending state) output-pending))
+      (values (- input-index input-start) (- output-index output-start)))))
+
+(defun octets->octets/base85 (state output input
+                              output-start output-end
+                              input-start input-end lastp)
+  (declare (type simple-octet-vector output))
+  (declare (optimize speed))
+  (base85-encoder state output input output-start output-end
+                  input-start input-end lastp #'char-code))
+
+(defun octets->string/base85 (state output input
+                              output-start output-end
+                              input-start input-end lastp)
+  (declare (type simple-string output))
+  (declare (optimize speed))
+  (base85-encoder state output input output-start output-end
+                  input-start input-end lastp #'identity))
 
 (defun encode-octets-base85 (octets start end table writer)
   (declare (type (simple-array (unsigned-byte 8) (*)) octets))
