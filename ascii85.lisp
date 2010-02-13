3be47ec23734f8a4cb968d2bbf46dd34aee77cdd
Nathan Froyd <froydnj@gmail.com>
2010-02-13 15:29:55 -0500

    add ascii85 decoding infrastructure

--- ascii85.lisp
+++ ascii85.lisp
@@ -150,14 +150,134 @@
   (ascii85-encoder state output input output-start output-end
                    input-start input-end lastp #'identity))
 
-(defun string->octets/ascii85 ()
-  )
-
-(defun octets->octets/decode/ascii85 ()
-  )
-
 (defvar *ascii85-decode-table* (make-decode-table *ascii85-encode-table*))
 (declaim (type decode-table *ascii85-decode-table*))
+
+(defstruct (ascii85-decode-state
+             (:include decode-state)
+             (:copier nil)
+             (:constructor %make-ascii85-decode-state
+                           (&aux (descriptor (ascii85-format-descriptor)))))
+  (bits 0 :type (unsigned-byte 32))
+  (pending 0 :type (integer 0 5))
+  (output-pending 0 :type (integer 0 4))
+  (table *ascii85-decode-table* :read-only t :type decode-table))
+
+(defun make-ascii85-decode-state (case-fold map01)
+  (declare (ignore case-fold map01))
+  (%make-ascii85-decode-state))
+
+(defun ascii85-decoder (state output input
+                        output-index output-end
+                        input-index input-end lastp converter)
+  (declare (type ascii85-decode-state state))
+  (declare (type simple-octet-vector output))
+  (declare (type index output-index output-end input-index input-end))
+  (declare (type function converter))
+  (let ((bits (ascii85-decode-state-bits state))
+        (pending (ascii85-decode-state-pending state))
+        (output-pending (ascii85-decode-state-output-pending state))
+        (table (ascii85-decode-state-table state)))
+    (declare (type (unsigned-byte 32) bits))
+    (declare (type (integer 0 5) pending))
+    (declare (type (integer 0 4) output-pending))
+    (tagbody
+     FINISHED-CHECK
+       (when (ascii85-decode-state-finished-input-p state)
+         (go FLUSH-BITS))
+     OUTPUT-AVAILABLE-CHECK
+       (when (zerop output-pending)
+         (go INPUT-AVAILABLE-CHECK))
+     OUTPUT-SPACE-CHECK
+       (when (>= output-index output-end)
+         (go DONE))
+     DO-OUTPUT
+       (setf (aref output output-index)
+             (ldb (byte 8 (* (decf output-pending) 8)) bits))
+       (incf output-index)
+       (cond
+         ((zerop output-pending)
+          (setf bits 0)
+          (setf pending 0)
+          (setf output-pending 0)
+          (go INPUT-AVAILABLE-CHECK))
+         (t
+          (go OUTPUT-SPACE-CHECK)))
+     INPUT-AVAILABLE-CHECK
+       (when (>= input-index input-end)
+         (go DONE))
+     DO-INPUT
+       (cond
+         ((< pending 5)
+          (let* ((c (aref input input-index))
+                 (v (funcall converter c))
+                 (d (dtref table v)))
+            (cond
+              ((eql c (funcall converter #\z))
+               (unless (zerop pending)
+                 (error "z found in the middle of an ascii85 group"))
+               (incf input-index)
+               (setf output-pending 4)
+               (go OUTPUT-SPACE-CHECK))
+              ((= d +dt-invalid+)
+               (error "invalid ascii85 character ~A at position ~D" c input-index))
+              (t
+               ;; FIXME: check for overflow.
+               (setf bits (+ (* bits 85) d))
+               (incf pending)
+               (incf input-index)
+               (go INPUT-AVAILABLE-CHECK)))))
+         (t
+          (setf output-pending 4)
+          (go OUTPUT-SPACE-CHECK)))
+     DONE
+       (unless lastp
+         (go RESTORE-STATE))
+       (setf (ascii85-decode-state-finished-input-p state) t)
+       ;; We should *always* have a complete group or nothing at this
+       ;; point.
+     EOT-VALIDITY-CHECK
+       (when (<= 1 pending 4)
+         (error "invalid ascii85 input"))
+       (setf output-pending (if (zerop pending) 0 4))
+     FLUSH-BITS
+       (when (zerop output-pending)
+         (go RESTORE-STATE))
+     FLUSH-OUTPUT-CHECK
+       (when (>= output-index output-end)
+         (go RESTORE-STATE))
+     DO-FLUSH-OUTPUT
+       (when (> output-pending 0)
+         (setf (aref output output-index)
+               (ldb (byte 8 (* (decf output-pending) 8)) bits))
+         (incf output-index)
+         (cond
+           ((zerop output-pending)
+            (setf bits 0)
+            (setf pending 0)
+            (setf output-pending 0)
+            (go RESTORE-STATE))
+           (t
+            (go FLUSH-OUTPUT-CHECK))))
+     RESTORE-STATE
+       (setf (ascii85-decode-state-bits state) bits
+             (ascii85-decode-state-pending state) pending
+             (ascii85-decode-state-output-pending state) output-pending))
+    (values input-index output-index)))
+
+(defun string->octets/ascii85 (state output input
+                              output-index output-end
+                              input-index input-end lastp)
+  (declare (type simple-string input))
+  (ascii85-decoder state output input output-index output-end
+                   input-index input-end lastp #'char-code))
+
+(defun octets->octets/decode/ascii85 (state output input
+                                     output-index output-end
+                                     input-index input-end lastp)
+  (declare (type simple-octet-vector input))
+  (ascii85-decoder state output input output-index output-end
+                   input-index input-end lastp #'identity))
 
 (defun decoded-length-ascii85 (length)
   ;; FIXME: There's nothing smart we can do without scanning the string.
@@ -213,4 +333,4 @@
 
 (register-descriptor-and-constructors :ascii85 (ascii85-format-descriptor)
                                       #'make-ascii85-encode-state
-                                      #'make-ascii85-encode-state)
+                                      #'make-ascii85-decode-state)
