85235ce740e9cef2fe5b3b59ddeb7c2cdf61fd57
Nathan Froyd <froydnj@gmail.com>
2010-02-13 15:32:15 -0500

    add base32 decoding infrastructure

--- base32.lisp
+++ base32.lisp
@@ -129,19 +129,149 @@
   (base32-encoder state output input output-start output-end
                   input-start input-end lastp #'identity))
 
-(defun string->octets/base32 ()
-  )
+(defvar *base32-decode-table* (make-decode-table *base32-encode-table*))
+(defvar *base32hex-decode-table* (make-decode-table *base32hex-encode-table*))
+(declaim (type decode-table *base32-decode-table* *base32hex-decode-table*))
 
-(defun octets->octets/decode/base32 ()
-  )
+(defun base32-decode-table (case-fold map01)
+  (let ((table *base32-decode-table*))
+    (when map01
+      (setf table (copy-seq table))
+      (setf (aref table (char-code #\0)) (aref table (char-code #\O)))
+      (case map01
+        ((#\I #\L) (setf (aref table (char-code #\1))
+                         (aref table (char-code map01))))))
+    (when case-fold
+      (setf table (case-fold-decode-table table *base32-encode-table*)))
+    table))
+
+(defstruct (base32-decode-state
+             (:include decode-state)
+             (:copier nil)
+             (:constructor %make-base32-decode-state
+                           (table
+                            &aux (descriptor (base32-format-descriptor)))))
+  (bits 0 :type (unsigned-byte 16))
+  (n-bits 0 :type (unsigned-byte 8))
+  (padding-remaining 0 :type (integer 0 6))
+  (table *base32-decode-table* :read-only t :type decode-table))
+
+(defun make-base32-decode-state (case-fold map01)
+  (%make-base32-decode-state (base32-decode-table case-fold map01)))
+
+(defun make-base32hex-decode-state (case-fold map01)
+  (declare (ignore case-fold map01))
+  (%make-base32-decode-state *base32hex-decode-table*))
+
+(defun base32-decoder (state output input
+                       output-index output-end
+                       input-index input-end lastp converter)
+  (declare (type base32-decode-state state))
+  (declare (type simple-octet-vector output))
+  (declare (type index output-index output-end input-index input-end))
+  (declare (type function converter))
+  (let ((bits (base32-decode-state-bits state))
+        (n-bits (base32-decode-state-n-bits state))
+        (padding-remaining (base32-decode-state-padding-remaining state))
+        (table (base32-decode-state-table state)))
+    (declare (type (unsigned-byte 16) bits))
+    (declare (type fixnum n-bits))
+    (declare (type (integer 0 6) padding-remaining))
+    (tagbody
+     PAD-CHECK
+       (when (base32-decode-state-finished-input-p state)
+         (go EAT-EQUAL-CHECK-PAD))
+     OUTPUT-AVAILABLE-CHECK
+       (when (< n-bits 8)
+         (go INPUT-AVAILABLE-CHECK))
+     OUTPUT-SPACE-CHECK
+       (when (>= output-index output-end)
+         (go DONE))
+     DO-OUTPUT
+       (decf n-bits 8)
+       (setf (aref output output-index) (logand (ash bits (- n-bits)) #xff)
+             bits (logand bits #xff))
+       (incf output-index)
+       (go INPUT-AVAILABLE-CHECK)
+     INPUT-AVAILABLE-CHECK
+       (when (>= input-index input-end)
+         (go DONE))
+     DO-INPUT
+       (let* ((c (aref input input-index))
+              (v (funcall converter c))
+              (d (dtref table v)))
+         (when (= v (funcall converter #\=))
+           (go SAW-EQUAL))
+         (when (= d +dt-invalid+)
+           (error "invalid base32 character ~A at position ~D" c input-index))
+         (incf input-index)
+         (setf bits (ldb (byte 16 0) (logior (ash bits 5) d)))
+         (incf n-bits 5)
+         (go OUTPUT-AVAILABLE-CHECK))
+     DONE
+       (unless lastp
+         (go RESTORE-STATE))
+     SAW-EQUAL
+       (setf (base32-decode-state-finished-input-p state) t)
+       ;; A complete base32 group is:
+       ;;
+       ;; vvvvvvvv wwwwwwww xxxxxxxx yyyyyyyy zzzzzzzz
+       ;;
+       ;; which gets encoded by:
+       ;;
+       ;; vvvvv vvvww wwwww wxxxx xxxxy yyyyy yyzzz zzzzz
+       ;;
+       ;; so the intermediate bits left are: 3 1 4 2 0
+       ;; corresponding to padding amounts : 6 4 3 1 0 (in characters)
+       ;;
+       ;; but we also have to handle cases where we start padding too
+       ;; soon: we can't handle padding after seeing 1 group of 5, 3
+       ;; groups of 5, 4 groups of 5 or 6 groups of five.  those
+       ;; correspond to 5 bits remaining (having not seen the 3 v's), 7
+       ;; bits remaining (having not seen the 1 w), 4 bits remaining
+       ;; (having not seen the 4 x's), and 6 bits remaining (having not
+       ;; seen the 2 y's).
+       (let ((n-pad-chars #.(make-array 5 :initial-contents '(0 4 1 6 3)
+                                        :element-type 'fixnum)))
+         (if (<= n-bits 4)
+             (setf padding-remaining (aref n-pad-chars n-bits))
+             (error "invalid base32 input")))
+     EAT-EQUAL-CHECK-PAD
+       (when (zerop padding-remaining)
+         (go RESTORE-STATE))
+     EAT-EQUAL-CHECK-INPUT
+       (when (>= input-index input-end)
+         (go RESTORE-STATE))
+     EAT-EQUAL
+       (let ((v (aref input input-index)))
+         (unless (= (funcall converter v) (funcall converter #\=))
+           (error "invalid base32 input ~A at position ~D" v input-index))
+         (incf input-index)
+         (decf padding-remaining)
+         (go EAT-EQUAL-CHECK-PAD))
+     RESTORE-STATE
+       (setf (base32-decode-state-n-bits state) n-bits
+             (base32-decode-state-bits state) bits
+             (base32-decode-state-padding-remaining state) padding-remaining))
+    (values input-index output-index)))
+
+(defun string->octets/base32 (state output input
+                              output-index output-end
+                              input-index input-end lastp)
+  (declare (type simple-string input))
+  (base32-decoder state output input output-index output-end
+                  input-index input-end lastp #'char-code))
+
+(defun octets->octets/decode/base32 (state output input
+                                     output-index output-end
+                                     input-index input-end lastp)
+  (declare (type simple-octet-vector input))
+  (base32-decoder state output input output-index output-end
+                  input-index input-end lastp #'identity))
 
 (defun encoded-length/base32 (count)
   "Return the number of characters required to encode COUNT octets in Base32."
   (* (ceiling count 5) 8))
-
-(defvar *base32-decode-table* (make-decode-table *base32-encode-table*))
-(defvar *base32hex-decode-table* (make-decode-table *base32hex-encode-table*))
-(declaim (type decode-table *base32-decode-table* *base32hex-decode-table*))
 
 (defun decode-octets-base32 (string start end length table writer)
   (declare (type index start end))
@@ -193,7 +323,7 @@
 
 (register-descriptor-and-constructors :base32 (base32-format-descriptor)
                                       #'make-base32-encode-state
-                                      #'make-base32-encode-state)
+                                      #'make-base32-decode-state)
 (register-descriptor-and-constructors :base32hex (base32-format-descriptor)
                                       #'make-base32hex-encode-state
-                                      #'make-base32hex-encode-state)
+                                      #'make-base32hex-decode-state)
